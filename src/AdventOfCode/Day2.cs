1371fd126eaf5a093f270281e65680b9bd492cfa
Adam Rodger <adam.rodger@gmail.com>
2019-12-02 08:08:34 +0000

    Day 2 🎅🎅🎅

---
+++ src/AdventOfCode/Day2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 2
+    /// </summary>
+    public class Day2
+    {
+        public int Part1(string[] input)
+        {
+            return RunIntCodeProgram(input, 12, 2);
+        }
+
+        public int Part2(string[] input)
+        {
+            const int target = 19690720;
+
+            for (int noun = 0; noun <= 99; noun++)
+            {
+                for (int verb = 0; verb <= 99; verb++)
+                {
+                    int result = RunIntCodeProgram(input, noun, verb);
+
+                    if (result == target)
+                    {
+                        return noun * 100 + verb;
+                    }
+                }
+            }
+
+            throw new Exception("not found");
+        }
+
+        private static int RunIntCodeProgram(string[] input, int noun, int verb)
+        {
+            int[] program = input.First().Split(',').Select(int.Parse).ToArray();
+
+            program[1] = noun;
+            program[2] = verb;
+
+            int counter = 0;
+
+            while (true)
+            {
+                int instruction = program[counter];
+
+                if (instruction == 99)
+                {
+                    return program[0];
+                }
+
+                int a = program[counter + 1];
+                int b = program[counter + 2];
+                int c = program[counter + 3];
+
+                switch (instruction)
+                {
+                    case 1:
+                        program[c] = program[a] + program[b];
+                        break;
+                    case 2:
+                        program[c] = program[a] * program[b];
+                        break;
+                }
+
+                counter += 4;
+            }
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day2Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day2Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day2 solver;
+
+        public Day2Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day2();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day2.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 6627023;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 2 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 4019;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 2 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
