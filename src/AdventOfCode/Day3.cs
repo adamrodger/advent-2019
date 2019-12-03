16893235c541a9f73c4cf7ee4c2443ca9563073a
Adam Rodger <adam.rodger@gmail.com>
2019-12-03 07:32:20 +0000

    Day 3 🤶🤶🤶

---
+++ src/AdventOfCode/Day3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 3
+    /// </summary>
+    public class Day3
+    {
+        public int Part1(string[] input)
+        {
+            return ClosestIntersection(input, (_, __, i) => Math.Abs(i.x) + Math.Abs(i.y));
+        }
+
+        public int Part2(string[] input)
+        {
+            return ClosestIntersection(input, (firstPath, secondPath, i) => firstPath.IndexOf(i) + 1 + secondPath.IndexOf(i) + 1);
+        }
+
+        private static int ClosestIntersection(string[] input, Func<IList<(int x, int y)>, IList<(int x, int y)>, (int x, int y), int> distanceFactory)
+        {
+            var firstDirections = Parse(input[0]);
+            var firstPath = BuildPath(firstDirections).ToList();
+
+            var secondDirections = Parse(input[1]);
+            var secondPath = BuildPath(secondDirections).ToList();
+
+            ICollection<(int x, int y)> intersections = firstPath.Intersect(secondPath).ToArray();
+
+            var distances = intersections.Select(i => distanceFactory(firstPath, secondPath, i));
+            return distances.Min();
+        }
+
+        private static ICollection<(char direction, int distance)> Parse(string input)
+        {
+            return input.Split(',').Select(s => (s[0], int.Parse(s.Substring(1)))).ToArray();
+        }
+
+        private static IEnumerable<(int x, int y)> BuildPath(ICollection<(char direction, int distance)> steps)
+        {
+            (int x, int y) current = (0, 0);
+
+            foreach ((char direction, int distance) in steps)
+            {
+                for (int i = 0; i < distance; i++)
+                {
+                    switch (direction)
+                    {
+                        case 'U':
+                            current = (current.x, current.y + 1);
+                            yield return current;
+                            break;
+                        case 'D':
+                            current = (current.x, current.y - 1);
+                            yield return current;
+                            break;
+                        case 'L':
+                            current = (current.x - 1, current.y);
+                            yield return current;
+                            break;
+                        case 'R':
+                            current = (current.x + 1, current.y);
+                            yield return current;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day3Tests.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day3Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day3 solver;
+
+        public Day3Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day3();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day3.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+
+            };
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 1285;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 3 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 14228;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 3 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
