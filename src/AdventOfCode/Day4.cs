79d6ae7e00f25132b17404d5e80afa1212063ff6
Adam Rodger <adam.rodger@gmail.com>
2019-12-04 08:08:13 +0000

    Day 4 🎁🎁🎁

---
+++ src/AdventOfCode/Day4.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 4
+    /// </summary>
+    public class Day4
+    {
+        private static readonly Regex pairRegex = new Regex("(\\d)\\1+", RegexOptions.Compiled | RegexOptions.Singleline);
+
+        public int Part1(string[] input)
+        {
+            return ValidPasswords(input).Count();
+        }
+
+        public int Part2(string[] input)
+        {
+            IEnumerable<string> matches = ValidPasswords(input);
+
+            // contains at least one 'exact' double - i.e. 22 instead of 222
+            return matches.Count(m => pairRegex.Matches(m).OfType<Match>().Any(g => g.Length == 2));
+        }
+
+        private static IEnumerable<string> ValidPasswords(string[] input)
+        {
+            int[] numbers = input[0].Split('-').Select(int.Parse).ToArray();
+
+            var matches = Enumerable.Range(numbers[0], numbers[1] - numbers[0])
+                                    .Select(i => i.ToString())
+                                    .Where(s => s[0] <= s[1] && s[1] <= s[2] && s[2] <= s[3] && s[3] <= s[4] && s[4] <= s[5])
+                                    .Where(s => pairRegex.IsMatch(s));
+            return matches;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day4Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day4Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day4 solver;
+
+        public Day4Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day4();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day4.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 594;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 4 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 364;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 4 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
