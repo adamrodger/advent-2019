6529e821b3909b76fc7b478e648f19059ecd6d04
Adam Rodger <adam.rodger@gmail.com>
2019-12-06 07:40:22 +0000

    Day 6 🕯️🕯️🕯️

---
+++ src/AdventOfCode/Day6.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 6
+    /// </summary>
+    public class Day6
+    {
+        public int Part1(string[] input)
+        {
+            var nodes = input.Select(i => new Node(i)).ToDictionary(n => n.Id);
+
+            return nodes.Values.Sum(n => n.PathToRoot(nodes).Count());
+        }
+
+        public int Part2(string[] input)
+        {
+            var nodes = input.Select(i => new Node(i)).ToDictionary(n => n.Id);
+
+            Node you = nodes["YOU"];
+            Node san = nodes["SAN"];
+
+            var youPath = you.PathToRoot(nodes).ToList();
+            var sanPath = san.PathToRoot(nodes).ToList();
+
+            // subtracting the common nodes gives 2 shorter paths to a common ancestor
+            var common = youPath.Intersect(sanPath).ToList();
+
+            // -1 because the you/san nodes themselves don't count
+            return (youPath.Count - common.Count - 1) + (sanPath.Count - common.Count - 1);
+        }
+    }
+
+    public class Node
+    {
+        public string Id { get; }
+
+        public string Parent { get; }
+
+        public Node(string input)
+        {
+            var split = input.Split(')');
+            this.Id = split[1];
+            this.Parent = split[0];
+        }
+
+        public IEnumerable<Node> PathToRoot(IDictionary<string, Node> graph)
+        {
+            yield return this;
+
+            string parent = this.Parent;
+
+            while (graph.ContainsKey(parent))
+            {
+                yield return graph[parent];
+                parent = graph[parent].Parent;
+            }
+        }
+
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            return $"{nameof(this.Id)}: {this.Id}, {nameof(this.Parent)}: {this.Parent}";
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day6Tests.cs
@@ -0,0 +1,74 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day6Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day6 solver;
+
+        public Day6Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day6();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day6.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                "COM)B",
+                "B)C",
+                "C)D",
+                "D)E",
+                "E)F",
+                "B)G",
+                "G)H",
+                "D)I",
+                "E)J",
+                "J)K",
+                "K)L",
+            };
+        }
+
+        [Fact]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = 42;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 254447;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 6 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 445;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 6 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
