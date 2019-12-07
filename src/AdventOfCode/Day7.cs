6fc5ec77e45e04990e94e401a79c2d01d9ede548
Adam Rodger <adam.rodger@gmail.com>
2019-12-07 08:00:51 +0000

    Day 7 part 1

---
+++ src/AdventOfCode/Day7.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdventOfCode.IntCode;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 7
+    /// </summary>
+    public class Day7
+    {
+        public int Part1(string[] input)
+        {
+            IEnumerable<IList<int>> permutations = Enumerable.Range(0, 5).ToArray().Permutations();
+            int max = -1;
+
+            foreach (IList<int> permutation in permutations)
+            {
+                int output = 0;
+
+                foreach (int thruster in permutation)
+                {
+                    var stdin = new Queue<int>(new[] {thruster, output});
+                    var stdout = new StringBuilder();
+
+                    var emulator = new IntCodeEmulator(input, stdin, stdout);
+                    emulator.Execute();
+
+                    output = int.Parse(stdout.ToString());
+                }
+
+                if (output > max)
+                {
+                    max = output;
+                }
+            }
+
+            return max;
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day7Tests.cs
@@ -0,0 +1,75 @@
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day7Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day7 solver;
+
+        public Day7Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day7();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day7.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                
+            };
+        }
+
+        [Fact]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 7 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 7 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
