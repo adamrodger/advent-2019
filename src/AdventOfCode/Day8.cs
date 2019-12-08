2f3f598987800f5eaf42c72b9cab02ec23ee3639
Adam Rodger <adam.rodger@gmail.com>
2019-12-08 07:30:18 +0000

    Day 8 🌟🌟🌟

---
+++ src/AdventOfCode/Day8.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Utilities;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 8
+    /// </summary>
+    public class Day8
+    {
+        private const int Width = 25;
+        private const int Height = 6;
+
+        public int Part1(string[] input)
+        {
+            string image = input[0];
+            char[] minLayer = image.Batch(Width * Height)
+                                   .MinBy(layer => layer.Count(c => c == '0'))
+                                   .First()
+                                   .ToArray();
+
+            return minLayer.Count(c => c == '1') * minLayer.Count(c => c == '2');
+        }
+
+        public string Part2(string[] input)
+        {
+            string image = input[0];
+            char[,] output = new char[Height, Width];
+
+            string[] layers = image.Batch(Width * Height).Select(b => new string(b.ToArray())).ToArray();
+
+            // 3d representation of (z,y,x) of each pixel in each layer/row/column
+            List<List<char[]>> layerRows = layers.Select(layer => layer.Batch(Width)
+                                                                       .Select(b => b.ToArray())
+                                                                       .ToList())
+                                                 .ToList();
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    var colour = layerRows.Select(l => l[y][x]).First(c => c != '2');
+                    output[y, x] = colour == '1' ? 'B' : ' ';
+                }
+            }
+
+            return output.Print();
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day8Tests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using AdventOfCode.Utilities;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day8Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day8 solver;
+
+        public Day8Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day8();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day8.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 1690;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 8 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            string expected = string.Join(Environment.NewLine,
+                "BBBB BBB  BBBB B  B BBB  ",
+                "   B B  B    B B  B B  B ",
+                "  B  B  B   B  B  B BBB  ",
+                " B   BBB   B   B  B B  B ",
+                "B    B    B    B  B B  B ",
+                "BBBB B    BBBB  BB  BBB  ",
+                Environment.NewLine);
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 8 - Part 2 -\n{result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
