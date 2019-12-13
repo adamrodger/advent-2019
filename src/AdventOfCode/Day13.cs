ea9a0dda4a553957ec35f2521d204cbe22a42045
Adam Rodger <adam.rodger@gmail.com>
2019-12-13 06:33:06 +0000

    Day 13 part 1

---
+++ src/AdventOfCode/Day13.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.IntCode;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 13
+    /// </summary>
+    public class Day13
+    {
+        public int Part1(string[] input)
+        {
+            var vm = new IntCodeEmulator(input);
+            vm.Execute();
+
+            return vm.StdOut.Batch(3).Count(b => b.ElementAt(2) == 2);
+
+            // 391 -- too high
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day13Tests.cs
@@ -0,0 +1,65 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day13Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day13 solver;
+
+        public Day13Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day13();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day13.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                
+            };
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 326;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 13 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 13 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
