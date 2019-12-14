6fdc8375d82c3da6cf749cf1c763521428ec90fa
Adam Rodger <adam.rodger@gmail.com>
2019-12-14 15:07:50 +0000

    Day 14 🧦🧦🧦

---
+++ src/AdventOfCode/Day14.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 14
+    /// </summary>
+    public class Day14
+    {
+        public long Part1(string[] input, long requiredFuel = 1)
+        {
+            var reactions = new Dictionary<string, Reaction>(input.Length);
+            var got = new Dictionary<string, long>(reactions.Count);
+            var needed = new Dictionary<string, long>(reactions.Count);
+
+            // parse reactions
+            foreach (string line in input)
+            {
+                MatchCollection matches = Regex.Matches(line, @"(\d+) ([A-Z]+)");
+                (int quantity, string chemical)[] parsed = matches.Cast<Match>().Select(m => (int.Parse(m.Groups[1].Value), m.Groups[2].Value)).ToArray();
+
+                (int quantity, string chemical) output = parsed.Last();
+
+                reactions[output.chemical] = new Reaction
+                {
+                    Inputs = parsed.Take(parsed.Length - 1).ToArray(),
+                    Output = output
+                };
+
+                needed[output.chemical] = 0;
+                got[output.chemical] = 0;
+            }
+
+            needed["ORE"] = 0;
+            needed["FUEL"] = requiredFuel;
+
+            React("FUEL", reactions, got, needed);
+            
+            return needed["ORE"];
+        }
+
+        public long Part2(string[] input)
+        {
+            const long target = 1000000000000;
+
+            // establish rough upper bound as power of 2
+            long max = 1;
+            while (this.Part1(input, max) < target)
+            {
+                max *= 2;
+            }
+
+            // converge on correct answer using binary chop
+            long min = 0;
+            while (min < max - 1)
+            {
+                long mid = (min + max) / 2;
+                long required = this.Part1(input, mid);
+
+                if (required < target)
+                {
+                    // too low, chop to the higher half
+                    min = mid;
+                }
+                else if (required > target)
+                {
+                    // too high, chop to the lower half
+                    max = mid;
+                }
+            }
+
+            return min;
+        }
+
+        /// <summary>
+        /// Work out how much of each chemical is needed to produce the needed amount of fuel
+        /// </summary>
+        /// <param name="chemical">Current chemical to produce (if necessary)</param>
+        /// <param name="reactions">Reaction formulae</param>
+        /// <param name="got">Stock if chemicals already produced</param>
+        /// <param name="needed">Amount of each chemical yet to be produced</param>
+        private static void React(string chemical, Dictionary<string, Reaction> reactions, Dictionary<string, long> got, Dictionary<string, long> needed)
+        {
+            if (!reactions.ContainsKey(chemical))
+            {
+                // input is a raw material - no reaction required
+                return;
+            }
+
+            Reaction reaction = reactions[chemical];
+
+            // can't react fractional amounts so round up to next whole number
+            double fractionalMultiplier = (double)(needed[chemical] - got[chemical]) / reaction.Output.quantity;
+            long multiplier = (long)Math.Ceiling(fractionalMultiplier);
+
+            // produce some more of it using the reaction formula in a depth-first recursive search
+            foreach ((int quantity, string chemical) input in reaction.Inputs)
+            {
+                needed[input.chemical] += input.quantity * multiplier;
+
+                // follow the reaction backwards to create the needed inputs until we hit raw material
+                React(input.chemical, reactions, got, needed);
+            }
+
+            got[chemical] += reaction.Output.quantity * multiplier;
+        }
+    }
+
+    public class Reaction
+    {
+        public ICollection<(int quantity, string chemical)> Inputs { get; set; }
+        public (int quantity, string chemical) Output { get; set; }
+
+        public override string ToString()
+        {
+            return $"{string.Join(", ", Inputs.Select(i => i.quantity + " " + i.chemical))} => {this.Output.quantity} {this.Output.chemical}";
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day14Tests.cs
@@ -0,0 +1,73 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day14Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day14 solver;
+
+        public Day14Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day14();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day14.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                "157 ORE => 5 NZVS",
+                "165 ORE => 6 DCFZ",
+                "44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL",
+                "12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ",
+                "179 ORE => 7 PSHF",
+                "177 ORE => 5 HKGWZ",
+                "7 DCFZ, 7 PSHF => 2 XJWVT",
+                "165 ORE => 2 GPVTF",
+                "3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT",
+            };
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 371695;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 14 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = 82892753;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 4052920;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 14 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
