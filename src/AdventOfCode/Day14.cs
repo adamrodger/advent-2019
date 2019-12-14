1fc25469d8e7302a3f668eaf4f4886bd5b394f0e
Adam Rodger <adam.rodger@gmail.com>
2019-12-14 07:34:20 +0000

    Day 14 part 1 too low

---
+++ src/AdventOfCode/Day14.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 14
+    /// </summary>
+    public class Day14
+    {
+        public int Part1(string[] input)
+        {
+            var reactions = new Dictionary<string, Reaction>(input.Length);
+            Dictionary<string, int> got = new Dictionary<string, int>(reactions.Count);
+            Dictionary<string, int> needed = new Dictionary<string, int>(reactions.Count);
+
+            foreach (string line in input)
+            {
+                MatchCollection matches = Regex.Matches(line, @"(\d+) ([A-Z]+)");
+                (int quantity, string chemical)[] parsed = matches.Cast<Match>().Select(m => (int.Parse(m.Groups[1].Value), m.Groups[2].Value)).ToArray();
+
+                (int quantity, string chemical) output = parsed.Last();
+
+                reactions[output.chemical] = new Reaction
+                {
+                    Inputs = parsed.Take(parsed.Length - 1).ToArray(),
+                    Output = output
+                };
+
+                needed[output.chemical] = 0;
+                got[output.chemical] = 0;
+            }
+
+            got["ORE"] = 0;
+            needed["ORE"] = 0;
+            
+            needed["FUEL"] = 1;
+
+            // recursively work backwards from FUEL, adding required amounts of input for each output
+            React("FUEL", reactions, got, needed);
+            
+            return needed["ORE"];
+
+            // guessed 174729 -- too low
+        }
+
+        public void React(string chemical, Dictionary<string, Reaction> reactions, Dictionary<string, int> got, Dictionary<string, int> needed)
+        {
+            Reaction reaction = reactions[chemical];
+            int multiplier = (needed[chemical] - got[chemical]) / reaction.Output.quantity;
+
+            // react up some more of it
+            got[chemical] += reaction.Output.quantity * multiplier;
+
+            foreach ((int quantity, string chemical) input in reaction.Inputs)
+            {
+                needed[input.chemical] += input.quantity * multiplier;
+
+                if (input.chemical != "ORE") // ORE has no inputs, don't follow backwards
+                {
+                    // follow the reaction backwards
+                    React(input.chemical, reactions, got, needed);
+                }
+            }
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+
+    public class Reaction
+    {
+        public ICollection<(int quantity, string chemical)> Inputs { get; set; }
+        public (int quantity, string chemical) Output { get; set; }
+
+        public override string ToString()
+        {
+            return $"{string.Join(", ", Inputs.Select(i => i.quantity + " " + i.chemical))} => {this.Output.quantity} {this.Output.chemical}";
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day14Tests.cs
@@ -0,0 +1,75 @@
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day14Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day14 solver;
+
+        public Day14Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day14();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day14.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                
+            };
+        }
+
+        [Fact]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 14 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 14 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
