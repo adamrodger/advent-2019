201101014e663baaad1c28710e406e7e5b233bae
Adam Rodger <adam.rodger@gmail.com>
2019-12-16 08:14:16 +0000

    Day 16 small tidy up whilst thinking about part 2

--- src/AdventOfCode/Day16.cs
+++ src/AdventOfCode/Day16.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using AdventOfCode.Utilities;
 using MoreLinq;
 
 namespace AdventOfCode
@@ -13,44 +12,48 @@ namespace AdventOfCode
     {
         public int Part1(string[] input)
         {
-            string line = input[0];
-            int[] basePattern = { 0, 1, 0, -1 };
+            string line = Transform(input[0]);
 
-            for (int x = 0; x < 100; x++)
-            {
-                var output = new StringBuilder(line.Length);
-
-                for (int i = 0; i < line.Length; i++)
-                {
-                    IEnumerable<int> pattern = basePattern.Repeat()
-                                                          .SelectMany(p => Enumerable.Repeat(p, i + 1))
-                                                          .Skip(1);
-
-                    var zipped = line.Zip(pattern, (c1, i1) => int.Parse(new string(new[] { c1 })) * i1);
-
-                    int a = zipped.Sum();
-                    int b = Math.Abs(a);
-                    int c = b % 10;
-
-                    output.Append(c.ToString());
-                }
-
-                line = output.ToString();
-            }
-
-            return int.Parse(new string(line.Take(8).ToArray()));
-
-            // guessed 59772698 -- too low
+            return int.Parse(line);
         }
 
         public int Part2(string[] input)
         {
-            foreach (string line in input)
+            string line = new string(input[0].Repeat(10000).ToArray());
+            int offset = int.Parse(new string(line.Take(7).ToArray()));
+
+            // 6,500,000 char string * 100 iterations == ouch. can't brute-force that
+            line = Transform(line, offset);
+
+            return int.Parse(line);
+        }
+
+        private static string Transform(string input, int offset = 0)
+        {
+            int[] basePattern = { 0, 1, 0, -1 };
+            int[] nums = input.Select(char.GetNumericValue).Select(c => (int)c).ToArray();
+
+            for (int x = 0; x < 100; x++)
             {
-                throw new NotImplementedException("Part 2 not implemented");
+                var output = new int[input.Length];
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    // got to be a mathsy way of calculating this instead of effectively reproducing all the time. This bit is slow
+                    IEnumerable<int> pattern = basePattern.Repeat()
+                                                          .SelectMany(p => Enumerable.Repeat(p, i + 1))
+                                                          .Skip(1);
+
+                    IEnumerable<int> zipped = nums.Zip(pattern, (n, p) => n * p);
+
+                    output[i] = zipped.Sum().Abs() % 10;
+                }
+
+                nums = output;
             }
 
-            return 0;
+            // turn nums back into a string
+            return string.Join(string.Empty, nums.Skip(offset).Take(8).Select(n => n.ToString()));
         }
     }
 }
