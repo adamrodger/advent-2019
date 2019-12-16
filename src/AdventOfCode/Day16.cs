b6f241dd7a57864c69c0ebdf87631f06d99b0b9b
Adam Rodger <adam.rodger@gmail.com>
2019-12-16 07:24:41 +0000

    Day 16 part 1 -- too low

---
+++ src/AdventOfCode/Day16.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 16
+    /// </summary>
+    public class Day16
+    {
+        public int Part1(string[] input)
+        {
+            string line = input[0];
+            int[] basePattern = { 0, 1, 0, -1 };
+
+            for (int x = 0; x < 100; x++)
+            {
+                StringBuilder output = new StringBuilder(line.Length);
+
+                for (int i = 0; i < line.Length; i++)
+                {
+                    IEnumerable<int> pattern = basePattern.Repeat()
+                                                          .SelectMany(p => Enumerable.Repeat(p, i + 1))
+                                                          .Skip(1);
+
+                    int a = (int)Convert.ChangeType(line[i], typeof(int)) * pattern.ElementAt(i);
+                    int b = Math.Abs(a);
+                    int c = b % 10;
+
+                    output.Append(c.ToString()[0]);
+                }
+
+                line = output.ToString();
+            }
+
+            return int.Parse(new string(line.Take(8).ToArray()));
+
+            // guessed 59772698 -- too low
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day16Tests.cs
@@ -0,0 +1,75 @@
+using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day16Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day16 solver;
+
+        public Day16Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day16();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day16.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new string[]
+            {
+                
+            };
+        }
+
+        [Fact]
+        public void Part1_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 16 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 16 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
