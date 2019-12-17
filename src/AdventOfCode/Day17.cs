7a6cd266e96c13e0f61f3613f05c9d91ecda8ce6
Adam Rodger <adam.rodger@gmail.com>
2019-12-17 07:49:55 +0000

    Day 17 part 1

---
+++ src/AdventOfCode/Day17.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using AdventOfCode.IntCode;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 17
+    /// </summary>
+    public class Day17
+    {
+        public int Part1(string[] input)
+        {
+            var vm = new IntCodeEmulator(input);
+            vm.Execute();
+
+            int dim = (int)Math.Ceiling(Math.Sqrt(vm.StdOut.Count));
+
+            char[,] grid = new char[dim,dim];
+            int x = 0, y = 0;
+
+            while(vm.StdOut.Any())
+            {
+                long value = vm.StdOut.Dequeue();
+
+                if (value == 10)
+                {
+                    x = 0;
+                    y++;
+                    continue;
+                }
+
+                if (value == 35)
+                {
+                    grid[y, x] = '#';
+                }
+                else if (value == 46)
+                {
+                    grid[y, x] = '.';
+                }
+
+                x++;
+            }
+
+            grid.Print();
+
+            int sum = 0;
+
+            grid.ForEach((x, y, c) =>
+            {
+                if (c == '#' && grid.Adjacent4(x, y).All(cell => cell == '#'))
+                {
+                    sum += x * y;
+                    grid[y, x] = 'O';
+                }
+            });
+
+            return sum;
+
+            // 1961 -- too high
+        }
+
+        public int Part2(string[] input)
+        {
+            var vm = new IntCodeEmulator(input);
+            vm.Program[0] = 2;
+
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day17Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day17Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day17 solver;
+
+        public Day17Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day17();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day17.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 1544;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 17 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 17 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
