cdb04e01808b52181fc10ac54ce077e67d8d83a3
Adam Rodger <adam.rodger@gmail.com>
2019-12-19 14:09:26 +0000

    Day 19 🤶🤶🤶

---
+++ src/AdventOfCode/Day19.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using AdventOfCode.IntCode;
+using AdventOfCode.Utilities;
+using MoreLinq.Extensions;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 19
+    /// </summary>
+    public class Day19
+    {
+        public int Part1(string[] input)
+        {
+            long[] program = input[0].Numbers<long>().Pad(input[0].Length + 100).ToArray();
+
+            int sum = 0;
+
+            for (int y = 0; y < 50; y++)
+            {
+                for (int x = 0; x < 50; x++)
+                {
+                    if (IsInRange(program, x, y))
+                    {
+                        sum++;
+                    }
+                }
+            }
+
+            return sum;
+        }
+
+        public int Part2(string[] input)
+        {
+            long[] program = input[0].Numbers<long>().Pad(input[0].Length + 100).ToArray();
+
+            // start a good way down
+            int x = 0;
+            int y = 1000;
+
+            while (true)
+            {
+                // walk x across until you hit the edge of the beam
+                while (!IsInRange(program, x, y))
+                {
+                    x++;
+                }
+
+                // check if it's big enough for a 100x100 square
+                if (IsInRange(program, x + 99, y - 99))
+                {
+                    return (x * 10000) + (y - 99);
+                }
+
+                // walk y down
+                y++;
+            }
+        }
+
+        private static bool IsInRange(long[] input, int x, int y)
+        {
+            var vm = new IntCodeEmulator(input);
+
+            vm.StdIn.Enqueue(x);
+            vm.StdIn.Enqueue(y);
+
+            vm.Execute();
+
+            return vm.StdOut.Dequeue() == 1;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day19Tests.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day19Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day19 solver;
+
+        public Day19Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day19();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day19.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 110;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 19 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 17302065;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 19 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
