74f23c93ec4dff7706698cd82d4a9f59af90b2ed
Adam Rodger <adam.rodger@gmail.com>
2019-12-21 07:34:07 +0000

    Day 21 🦌🦌🦌

---
+++ src/AdventOfCode/Day21.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using AdventOfCode.IntCode;
+using MoreLinq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 21
+    /// </summary>
+    public class Day21
+    {
+        public int Part1(string[] input)
+        {
+            var program = new[]
+            {
+                "OR A J",     // J = A
+                "AND B J",    // J = J || B
+                "AND C J",    // J = J || C
+                "NOT J J",    // J = !(A || B || C)
+                "AND D J",    // J = D && !(A || B || C)
+                "WALK"
+            };
+
+            return RunSpringScript(input, program);
+        }
+
+
+        public int Part2(string[] input)
+        {
+            var program = new[]
+            {
+                "OR A J",     // J = A
+                "AND B J",    // J = J || B
+                "AND C J",    // J = J || C
+                "NOT J J",    // J = !(A || B || C)
+                "AND D J",    // J = D && !(A || B || C)
+
+                "OR E T",     // T = E
+                "OR H T",     // T = E || H
+
+                "AND T J",    // J = (D && !(A || B || C)) && (E || H)
+                "RUN"
+            };
+
+            return RunSpringScript(input, program);
+        }
+
+        private static int RunSpringScript(string[] input, string[] program)
+        {
+            var vm = new IntCodeEmulator(input);
+            program.ForEach(p => InputInstruction(vm, p));
+            vm.Execute();
+
+            while (vm.StdOut.Any())
+            {
+                long output = vm.StdOut.Dequeue();
+
+                if (output > 255)
+                {
+                    return (int) output;
+                }
+
+                Debug.Write((char) output);
+            }
+
+            return int.MinValue;
+        }
+
+        private static void InputInstruction(IntCodeEmulator vm, string instruction)
+        {
+            foreach (char c in instruction)
+            {
+                vm.StdIn.Enqueue(c);
+            }
+
+            vm.StdIn.Enqueue('\n');
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day21Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day21Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day21 solver;
+
+        public Day21Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day21();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day21.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 19354928;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 21 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 1141997803;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 21 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
