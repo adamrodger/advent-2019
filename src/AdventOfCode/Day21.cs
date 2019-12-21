17772c5a6442f96988b93b64025646253033c914
Adam Rodger <adam.rodger@gmail.com>
2019-12-21 07:47:26 +0000

    Day 21 🦌🦌🦌

--- src/AdventOfCode/Day21.cs
+++ src/AdventOfCode/Day21.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
 using AdventOfCode.IntCode;
 using MoreLinq;
@@ -16,31 +15,32 @@ namespace AdventOfCode
             var program = new[]
             {
                 "OR A J",     // J = A
-                "AND B J",    // J = J || B
-                "AND C J",    // J = J || C
-                "NOT J J",    // J = !(A || B || C)
-                "AND D J",    // J = D && !(A || B || C)
+                "AND B J",    // J = A && B
+                "AND C J",    // J = A && B && C
+                "NOT J J",    // J = !(A && B && C)      - there is a hole somewhere in front of me
+
+                "AND D J",    // J = !(A && B && C) && D - and it's safe to jump over the hole
                 "WALK"
             };
 
             return RunSpringScript(input, program);
         }
 
-
         public int Part2(string[] input)
         {
             var program = new[]
             {
                 "OR A J",     // J = A
-                "AND B J",    // J = J || B
-                "AND C J",    // J = J || C
-                "NOT J J",    // J = !(A || B || C)
-                "AND D J",    // J = D && !(A || B || C)
+                "AND B J",    // J = A && B
+                "AND C J",    // J = A && B && C
+                "NOT J J",    // J = !(A && B && C)      - there is a hole somewhere in front of me
 
-                "OR E T",     // T = E
-                "OR H T",     // T = E || H
+                "AND D J",    // J = !(A && B && C) && D - and it's safe to jump over the hole
 
-                "AND T J",    // J = (D && !(A || B || C)) && (E || H)
+                "OR E T",     // T = E                   - can walk after the jump
+                "OR H T",     // T = E || H              - or can safely double-jump
+
+                "AND T J",    // J = !(A && B && C) && D && (E || H)
                 "RUN"
             };
 
