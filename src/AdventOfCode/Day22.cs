eaef891163eb5efc3bfd48c79c9c6bdef497217f
Adam Rodger <adam.rodger@gmail.com>
2019-12-22 07:23:56 +0000

    Day 22 part 1

---
+++ src/AdventOfCode/Day22.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using AdventOfCode.Utilities;
+using MoreLinq;
+using Nito.Collections;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 22
+    /// </summary>
+    public class Day22
+    {
+        public int Part1(string[] input, int N = 10007)
+        {
+            var deck = new Deque<int>(Enumerable.Range(0, N));
+
+            foreach (string instruction in input)
+            {
+                if (instruction == "deal into new stack")
+                {
+                    deck = deck.Reverse().ToDeque();
+                }
+                else if (instruction.StartsWith("cut"))
+                {
+                    int n = instruction.Numbers<int>().First();
+
+                    if (n < 0)
+                    {
+                        Enumerable.Range(0, n * -1).ForEach(i => deck.AddToFront(deck.RemoveFromBack()));
+                    }
+                    else
+                    {
+                        Enumerable.Range(0, n).ForEach(i => deck.AddToBack(deck.RemoveFromFront()));
+                    }
+                }
+                else if (instruction.StartsWith("deal with increment"))
+                {
+                    int n = instruction.Numbers<int>().First();
+
+                    int count = deck.Count;
+                    var newDeck = new int[count];
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        newDeck[(n * i) % count] = deck.RemoveFromFront();
+                    }
+
+                    deck = newDeck.ToDeque();
+                }
+            }
+
+            return deck.IndexOf(2019);
+
+            // 9525 -- too high
+            // 9403 -- too high
+            // 7753 -- too high
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+
+    public static class DequeExtensions
+    {
+        public static Deque<T> ToDeque<T>(this IEnumerable<T> @this)
+        {
+            return new Deque<T>(@this);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day22Tests.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day22Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day22 solver;
+
+        public Day22Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day22();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day22.txt");
+            return input;
+        }
+
+        private static string[] GetSampleInput()
+        {
+            return new[]
+            {
+                //"cut 6",
+                //"deal with increment 7",
+                //"deal into new stack",
+
+                "cut -3"
+            };
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 3036;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 22 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_SampleInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetSampleInput());
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 22 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
