dbebabdb3ed33f0787f0a718f4322c868f2bb279
Adam Rodger <adam.rodger@gmail.com>
2019-12-22 17:14:34 +0000

    Day 22 🍪🍪🍪

---
+++ src/AdventOfCode/Day22.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 22
+    /// </summary>
+    public class Day22
+    {
+        public int Part1(string[] input, int count = 10007)
+        {
+            int[] deck = Shuffle(input, count);
+
+            return Array.IndexOf(deck, 2019);
+        }
+
+        public BigInteger Part2(string[] input)
+        {
+            BigInteger d = 119315717514047; // deck size
+            BigInteger s = 101741582076661; // number of shuffles
+
+            BigInteger x = 2020;                        // the deck index we're tracking
+            BigInteger y = ReverseShuffle(input, d, x); // that card's location one shuffle back in time
+            BigInteger z = ReverseShuffle(input, d, y); // that card's location another shuffle back in time (i.e. 2 back from x)
+
+            // work out a and b in a*x+b, which is the simplified form of the reversing function equations
+            BigInteger a = (y - z) * ModInverse(x - y + d, d) % d;
+            BigInteger b = (y - a * x) % d;
+
+            // apply a*x+b for the known a, b and x
+            var result = (BigInteger.ModPow(a, s, d) * x + (BigInteger.ModPow(a, s, d) - 1) * ModInverse(a - 1, d) * b) % d;
+            return result;
+        }
+
+        /// <summary>
+        /// Shuffles a deck of the given size according to the given instructions
+        /// </summary>
+        /// <param name="input">Shuffling instructions</param>
+        /// <param name="count">Deck size</param>
+        /// <returns>Shuffled deck</returns>
+        public static int[] Shuffle(string[] input, int count)
+        {
+            var deck = Enumerable.Range(0, count).ToArray();
+
+            foreach (string instruction in input)
+            {
+                int[] newDeck;
+
+                if (instruction == "deal into new stack")
+                {
+                    newDeck = deck.Reverse().ToArray();
+                }
+                else if (instruction.StartsWith("cut"))
+                {
+                    int n = instruction.Numbers<int>().First();
+
+                    // equiv to (i - n) but wraps round negatives
+                    newDeck = Enumerable.Range(0, count).Select(i => deck[(count + i + n) % count]).ToArray(); 
+                }
+                else if (instruction.StartsWith("deal with increment"))
+                {
+                    int n = instruction.Numbers<int>().First();
+
+                    newDeck = new int[count];
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        newDeck[(i * n) % count] = deck[i];
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                deck = newDeck;
+            }
+
+            return deck;
+        }
+
+        /// <summary>
+        /// Reverses the shuffle at index <paramref name="i"/>
+        /// </summary>
+        /// <param name="instructions">Instructions for shuffle (to run in reverse)</param>
+        /// <param name="cards">Number of cards in the deck</param>
+        /// <param name="i">Deck position</param>
+        /// <returns>Starting position of the card currently at the given index (i.e. the shuffle has been reversed)</returns>
+        private static BigInteger ReverseShuffle(string[] instructions, BigInteger cards, BigInteger i)
+        {
+            foreach (string instruction in instructions.Reverse())
+            {
+                if (instruction == "deal into new stack")
+                {
+                    i = cards - 1 - i;
+                    continue;
+                }
+
+                int n = instruction.Numbers<int>().First();
+
+                if (instruction.StartsWith("cut"))
+                {
+                    i += n;
+                }
+                else if (instruction.StartsWith("deal with increment"))
+                {
+                    i = ModInverse(n, cards) * i % cards;
+                }
+            }
+
+            return i;
+        }
+
+        /// <summary>
+        /// https://stackoverflow.com/a/15768873
+        /// </summary>
+        public static BigInteger ModInverse(BigInteger a, BigInteger n)
+        {
+            return BigInteger.ModPow(a, n - 2, n);
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day22Tests.cs
@@ -0,0 +1,138 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day22Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day22 solver;
+
+        public Day22Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day22();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day22.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 3036;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 22 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 70618172909245;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 22 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Shuffle_Reverse_ReversesDeck()
+        {
+            int[] expected = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+
+            int[] actual = Day22.Shuffle(new[] { "deal into new stack" }, expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Shuffle_DoubleReverse_LeavesDeckTheSame()
+        {
+            int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            int[] actual = Day22.Shuffle(new[] { "deal into new stack", "deal into new stack" }, expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Shuffle_CutPositive_MovesCardsToTheBack()
+        {
+            int[] expected = {3, 4, 5, 6, 7, 8, 9, 0, 1, 2};
+
+            int[] actual = Day22.Shuffle(new[] { "cut 3" }, expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Shuffle_CutNegative_MovesCardsToTheFront()
+        {
+            int[] expected = {6, 7, 8, 9, 0, 1, 2, 3, 4, 5};
+
+            int[] actual = Day22.Shuffle(new[] { "cut -4" }, expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Shuffle_DoubleCut_LeavesDeckTheSame()
+        {
+            int[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+            int[] actual = Day22.Shuffle(new[] { "cut 3", "cut -3" }, expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Shuffle_Increment_ShufflesDeckCorrectly()
+        {
+            int[] expected = {0, 7, 4, 1, 8, 5, 2, 9, 6, 3};
+
+            int[] actual = Day22.Shuffle(new[] { "deal with increment 3" }, expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Shuffle_AllOperations_ShufflesDeck()
+        {
+            int[] expected = {3, 0, 7, 4, 1, 8, 5, 2, 9, 6};
+
+            int[] actual = Day22.Shuffle(new[]
+                                         {
+                                             "cut 6",
+                                             "deal with increment 7",
+                                             "deal into new stack"
+                                         },
+                                         expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Shuffle_MultipleOperations_ShufflesDeck()
+        {
+            int[] expected = { 0, 3, 6, 9, 2, 5, 8, 1, 4, 7 };
+
+            int[] actual = Day22.Shuffle(new[]
+                                         {
+                                             "deal with increment 7",
+                                             "deal into new stack",
+                                             "deal into new stack"
+                                         },
+                                         expected.Length);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
