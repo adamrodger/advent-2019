d9d6893641584b32a6006692ec1fe4f9cf8d041b
Adam Rodger <adam.rodger@gmail.com>
2019-12-23 06:39:40 +0000

    Day 23 part 1

---
+++ src/AdventOfCode/Day23.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using AdventOfCode.IntCode;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 23
+    /// </summary>
+    public class Day23
+    {
+        public long Part1(string[] input)
+        {
+            var vms = new Dictionary<long, IntCodeEmulator>(50);
+            var mailboxes = new Dictionary<long, Queue<long>>(50);
+
+            for (int id = 0; id < 50; id++)
+            {
+                var vm = new IntCodeEmulator(input);
+                vm.StdIn.Enqueue(id);
+                vm.ExecuteUntilYield();
+
+                mailboxes[id] = new Queue<long>();
+
+                vms[id] = vm;
+            }
+
+            while (true)
+            {
+                for (int id = 0; id < 50; id++)
+                {
+                    var vm = vms[id];
+                    var mailbox = mailboxes[id];
+
+                    if (!mailbox.Any())
+                    {
+                        mailbox.Enqueue(-1);
+                    }
+
+                    while (mailbox.Any())
+                    {
+                        vm.StdIn.Enqueue(mailbox.Dequeue());
+                    }
+
+                    vm.ExecuteUntilYield();
+
+                    while (vm.StdOut.Any())
+                    {
+                        var (dest, x, y) = (vm.StdOut.Dequeue(), vm.StdOut.Dequeue(), vm.StdOut.Dequeue());
+
+                        if (dest == 255)
+                        {
+                            return y;
+                        }
+
+                        mailboxes[dest].Enqueue(x);
+                        mailboxes[dest].Enqueue(y);
+                        
+                    }
+                }
+            }
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day23Tests.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+
+namespace AdventOfCode.Tests
+{
+    public class Day23Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day23 solver;
+
+        public Day23Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day23();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day23.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 15416;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 23 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Part2_RealInput_ProducesCorrectResponse()
+        {
+            var expected = -1;
+
+            var result = solver.Part2(GetRealInput());
+            output.WriteLine($"Day 23 - Part 2 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
