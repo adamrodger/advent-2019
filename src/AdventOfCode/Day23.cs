75d708ffb70e70dbcaa0c648e681b2f4b69d7fcb
Adam Rodger <adam.rodger@gmail.com>
2019-12-23 07:10:45 +0000

    Day 23 🎁🎁🎁

---
+++ src/AdventOfCode/Day23.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.IntCode;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 23
+    /// </summary>
+    public class Day23
+    {
+        public long Part1(string[] input)
+        {
+            Dictionary<long, IntCodeEmulator> vms = BuildVMs(input);
+
+            while (true)
+            {
+                for (int id = 0; id < 50; id++)
+                {
+                    var vm = vms[id];
+
+                    if (!vm.StdIn.Any())
+                    {
+                        vm.StdIn.Enqueue(-1);
+                    }
+
+                    vm.ExecuteUntilYield();
+
+                    while (vm.StdOut.Any())
+                    {
+                        var (dest, x, y) = (vm.StdOut.Dequeue(), vm.StdOut.Dequeue(), vm.StdOut.Dequeue());
+
+                        if (dest == 255)
+                        {
+                            return y;
+                        }
+
+                        vms[dest].StdIn.Enqueue(x);
+                        vms[dest].StdIn.Enqueue(y);
+                    }
+                }
+            }
+        }
+
+        public long Part2(string[] input)
+        {
+            Dictionary<long, IntCodeEmulator> vms = BuildVMs(input);
+
+            (long x, long y) nat = (0, 0);
+            (long x, long y) previous = (-99, -99);
+
+            while (true)
+            {
+                for (int id = 0; id < 50; id++)
+                {
+                    var vm = vms[id];
+
+                    if (!vm.StdIn.Any())
+                    {
+                        vm.StdIn.Enqueue(-1);
+                    }
+
+                    vm.ExecuteUntilYield();
+
+                    while (vm.StdOut.Any())
+                    {
+                        var (dest, x, y) = (vm.StdOut.Dequeue(), vm.StdOut.Dequeue(), vm.StdOut.Dequeue());
+
+                        if (dest == 255)
+                        {
+                            nat.x = x;
+                            nat.y = y;
+                        }
+                        else
+                        {
+                            vms[dest].StdIn.Enqueue(x);
+                            vms[dest].StdIn.Enqueue(y);
+                        }
+                    }
+                }
+
+                // check for idle
+                if (vms.Values.All(vm => !vm.StdIn.Any()))
+                {
+                    if (nat == previous)
+                    {
+                        // sent twice
+                        return nat.y;
+                    }
+                    vms[0].StdIn.Enqueue(nat.x);
+                    vms[0].StdIn.Enqueue(nat.y);
+
+                    previous = nat;
+                }
+            }
+        }
+
+        private static Dictionary<long, IntCodeEmulator> BuildVMs(string[] input)
+        {
+            var vms = new Dictionary<long, IntCodeEmulator>(50);
+
+            for (int id = 0; id < 50; id++)
+            {
+                var vm = new IntCodeEmulator(input);
+                vm.StdIn.Enqueue(id);
+                vm.ExecuteUntilYield();
+
+                vms[id] = vm;
+            }
+
+            return vms;
+        }
+    }
+}
