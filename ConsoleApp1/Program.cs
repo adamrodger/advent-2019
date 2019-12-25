e2b5058d3bf02bcb8ba4833e93dc0ccaf0a82ab7
Adam Rodger <adam.rodger@gmail.com>
2019-12-25 06:39:38 +0000

    Day 25 solved by hand. Will create program later

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using AdventOfCode.IntCode;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = File.ReadAllLines(args[0]);
+            var vm = new IntCodeEmulator(input);
+
+            while (true)
+            {
+                vm.ExecuteUntilYield();
+
+                while (vm.StdOut.Any())
+                {
+                    Console.Write((char)vm.StdOut.Dequeue());
+                }
+
+                string line = Console.ReadLine();
+                foreach (char c in line)
+                {
+                    vm.StdIn.Enqueue(c);
+                }
+
+                vm.StdIn.Enqueue(10);
+            }
+        }
+    }
+}
---
+++ src/AdventOfCode/Day25.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using AdventOfCode.IntCode;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 25
+    /// </summary>
+    public class Day25
+    {
+        public int Part1(string[] input)
+        {
+            var vm = new IntCodeEmulator(input);
+
+            while (true)
+            {
+                vm.ExecuteUntilYield();
+
+                while (vm.StdOut.Any())
+                {
+                    Debug.Write((char)vm.StdOut.Dequeue());
+                }
+
+                string line = Console.ReadLine();
+                foreach (char c in line)
+                {
+                    vm.StdIn.Enqueue(c);
+                }
+            }
+        }
+
+        public int Part2(string[] input)
+        {
+            foreach (string line in input)
+            {
+                throw new NotImplementedException("Part 2 not implemented");
+            }
+
+            return 0;
+        }
+    }
+}
