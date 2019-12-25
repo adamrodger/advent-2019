4022152e9038b31402fbc74446f262ffc2cb0732
Adam Rodger <adam.rodger@gmail.com>
2019-12-25 19:49:55 +0000

    Day 25 🎅🤶🦌🍪🥛🌟❄️☃️🎄🎁🔔🕯️

---
+++ src/AdventOfCode/Day25.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Text;
+using AdventOfCode.IntCode;
+using AdventOfCode.Utilities;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// Solver for Day 25
+    /// </summary>
+    public class Day25
+    {
+        public int Part1(string[] input)
+        {
+            var vm = new IntCodeEmulator(input);
+
+            var instructions = new[]
+            {
+                "west",
+                "south",
+                "east",
+                "take monolith",
+                "south",
+                "west",
+                "west",
+                "take astrolabe",
+                "east",
+                "east",
+                "north",
+                "west",
+                "north",
+                "west",
+                "north",
+                "take tambourine",
+                "south",
+                "west",
+                "take dark matter",
+                "west",
+                "north"
+            };
+
+            foreach (char c in string.Join('\n', instructions))
+            {
+                vm.StdIn.Enqueue(c);
+            }
+            vm.StdIn.Enqueue('\n');
+
+            vm.ExecuteUntilYield();
+
+            var output = new StringBuilder();
+            while (vm.StdOut.Any())
+            {
+               output.Append((char)vm.StdOut.Dequeue());
+            }
+
+            return output.ToString().Numbers<int>().Last();
+        }
+    }
+}
---
+++ tests/AdventOfCode.Tests/Day25Tests.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AdventOfCode.Tests
+{
+    public class Day25Tests
+    {
+        private readonly ITestOutputHelper output;
+        private readonly Day25 solver;
+
+        public Day25Tests(ITestOutputHelper output)
+        {
+            this.output = output;
+            this.solver = new Day25();
+        }
+
+        private static string[] GetRealInput()
+        {
+            string[] input = File.ReadAllLines("inputs/day25.txt");
+            return input;
+        }
+
+        [Fact]
+        public void Part1_RealInput_ProducesCorrectResponse()
+        {
+            var expected = 67635328;
+
+            var result = solver.Part1(GetRealInput());
+            output.WriteLine($"Day 25 - Part 1 - {result}");
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
